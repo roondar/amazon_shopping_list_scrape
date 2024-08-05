8c0cc8499a789d1bc6dbea39fb50bfbcb4539bbf
roondar <roondar@users.noreply.github.com>
2024-08-05 14:06:39 +0200

    Update scrapeAmazon.js

--- scrapeAmazon.js
+++ scrapeAmazon.js
@@ -3,10 +3,12 @@ const puppeteer = require('puppeteer');
 const OTPAuth = require('otpauth');  // For handling OTP
 const fs = require('fs');
 
-// Replace this with your actual secret key you get from the amazon add MFA page - and remove the spaces
+// Replace this with your actual secret key you get from the amazon add MFA page 
+// and remove the spaces
 const secret = process.env.AMZ_SECRET;
 const amz_login = process.env.AMZ_LOGIN;
 const amz_password = process.env.AMZ_PASS;
+const amz_domain = process.env.AMZ_DOMAIN;
 
 // Create a new OTPAuth instance
 const totp = new OTPAuth.TOTP({
@@ -35,7 +37,6 @@ async function getOTP(secret) {
 
 (async () => {
     const browser = await puppeteer.launch({
-//({ headless: false });
 	    headless: true,
 	    defaultViewport: null,
 	    executablePath: '/usr/bin/google-chrome',
@@ -44,7 +45,7 @@ async function getOTP(secret) {
     const page = await browser.newPage();
 
     // Navigate to Amazon login page
-    await page.goto('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=3600&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Falexaquantum%2Fsp%2FalexaShoppingList%3Fref_%3Dlist_d_wl_ys_list_1&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_alexa_quantum_us&openid.mode=checkid_setup&language=en_US&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0');
+    await page.goto(`https://${amz_domain}/ap/signin?openid.pape.max_auth_age=3600&openid.return_to=https%3A%2F%2F${amz_domain}%2Falexaquantum%2Fsp%2FalexaShoppingList%3Fref_%3Dlist_d_wl_ys_list_1&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_alexa_quantum_us&openid.mode=checkid_setup&language=en_US&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0`);
 
     // Enter username
     await page.type('#ap_email', amz_login);
@@ -54,7 +55,6 @@ async function getOTP(secret) {
     await page.click('#signInSubmit');
     await page.waitForNavigation();
 
-
     // Handle OTP (if required)
     if (await page.$('#auth-mfa-otpcode')) {
         await page.type('#auth-mfa-otpcode', token);
@@ -63,38 +63,37 @@ async function getOTP(secret) {
     }
 
     // Navigate to Alexa Shopping List page
-    await page.goto('https://www.amazon.com/alexaquantum/sp/alexaShoppingList?ref_=list_d_wl_ys_list_1');
+    await page.goto(`https://${amz_domain}/alexaquantum/sp/alexaShoppingList?ref_=list_d_wl_ys_list_1`);
 
     const pageContent = await page.content();
     sleep(3000, function() {
-    // delay
+      // delay
     });
 
-  let itemTitles = await page.$$eval(".virtual-list .item-title", items =>
-    items.map(item => item.textContent.trim())
-  );
+    let itemTitles = await page.$$eval(".virtual-list .item-title", items =>
+      items.map(item => item.textContent.trim())
+    );
 
-  // Format each item as <listItem>
-  let formattedItems = itemTitles.map(item => `${item}`);
+    // Format each item as <listItem>
+    let formattedItems = itemTitles.map(item => `${item}`);
 
-  // Convert the array to JSON format
-  let jsonFormattedItems = JSON.stringify(formattedItems, null, 2);
+    // Convert the array to JSON format
+    let jsonFormattedItems = JSON.stringify(formattedItems, null, 2);
 
-  
-  // Save the JSON formatted list to default.htm
-  const outputDir = '.';
-  if (!fs.existsSync(outputDir)){
-    fs.mkdirSync(outputDir, { recursive: true });
-  }
-  fs.writeFileSync(`${outputDir}/list_of_items.json`, jsonFormattedItems);
-	
+    // Save the JSON formatted list to default.htm
+    const outputDir = '.';
+    if (!fs.existsSync(outputDir)){
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+    fs.writeFileSync(`${outputDir}/list_of_items.json`, jsonFormattedItems);
+      
+    // Display the JSON formatted list
+    console.log(jsonFormattedItems);
 
-  // Display the JSON formatted list
-  console.log(jsonFormattedItems);
-
-  // Close the browser when done
+    // Close the browser when done
     await browser.close();
 })();
+
 function sleep(time, callback) {
   var stop = new Date().getTime();
   while (new Date().getTime() < stop + time) {
